f373bd17778e1a4b4578f3dadf39934ea3291cbb
pathum <awpathum@gmail.com>
2020-03-03 11:23:53 +0530

    Passengers servlet created

--- src/com/airline/service/PassengerService.java
+++ src/com/airline/service/PassengerService.java
@@ -1,9 +1,14 @@
 package com.airline.service;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+
+import javax.persistence.TypedQuery;
 
 import com.airline.models.Passenger;
 
@@ -27,5 +32,14 @@ public class PassengerService {
     public void addPassenger(Passenger p){
     	em.persist(p);
     }
-
+    
+    public List<Passenger> getPassengers(){
+    	
+    	TypedQuery<Passenger> query = em.createQuery("SELECT p FROM Passenger p",Passenger.class);
+    	
+    	List<Passenger>pList = query.getResultList();
+    	
+    	return pList;
+    }
+    
 }
