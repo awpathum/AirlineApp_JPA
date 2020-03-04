1f41c52792fd59d368c33e15eb0fd3d5cdc46c4d
pathum <awpathum@gmail.com>
2020-03-05 00:13:35 +0530

    AddPassenger bug fixed

--- src/com/airline/controllers/AddPassenger.java
+++ src/com/airline/controllers/AddPassenger.java
@@ -1,6 +1,7 @@
 package com.airline.controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -70,27 +71,40 @@ public class AddPassenger extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		
-		response.sendRedirect("Passengers");
+		//response.sendRedirect("Passengers");
 		
-//		Passenger p = new Passenger();
-//
-//		p.setFirstName("Pathum");
-//		p.setLastName("Wijethunge");
-//
-//		Calendar cal = Calendar.getInstance();
-//		cal.set(Calendar.YEAR, 1995);
-//		cal.set(Calendar.MONTH, 9);
-//		cal.set(Calendar.DAY_OF_MONTH, 15);
-//
-//		Date dob = cal.getTime();
-//		p.setDob(dob);
-//
-//		p.setGender(Gender.Male);
-//		p.setFlightclass(FlightClass.Coach);
-//		
-//		System.out.println(p);
-//		
-//		ps.addPassenger(p);
+		PrintWriter out = response.getWriter();
+		out.println("AddPassenger Called");
+		
+		String firstName = request.getParameter("first_name");
+		String lastName = request.getParameter("last_name");
+		String raw_dob = request.getParameter("dob");
+//		String year = request.getParameter("year");
+//		String month = request.getParameter("month");
+//		String day = request.getParameter("day");
+		String gender = request.getParameter("gender");
+		
+		String dobArr[] = raw_dob.split("\\/");
+		Passenger p = new Passenger();
+
+		p.setFirstName(firstName);
+		p.setLastName(lastName);
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, Integer.parseInt(dobArr[2]));
+		cal.set(Calendar.MONTH, Integer.parseInt(dobArr[1]));
+		cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dobArr[0]));
+
+		Date dob = cal.getTime();
+		p.setDob(dob);
+
+		p.setGender(Gender.valueOf(gender));
+		p.setFlightclass(FlightClass.Coach);
+		
+		System.out.println(p);
+		
+		ps.addPassenger(p);
+		response.sendRedirect("Passengers");
 	}
 
 }
