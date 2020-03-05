d044700d15d38807a49ca4c3758ee2d35cb535db
pathum <awpathum@gmail.com>
2020-03-05 11:48:05 +0530

    ApplicationPath created

---
+++ src/com/airline/webservices/rest/RestApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.airline.webservices.rest;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/airlineservices/rest")
+public class RestApplicationConfig extends javax.ws.rs.core.Application{
+	
+	
+
+}
